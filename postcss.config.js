c49363d53f0377eadd7ac7380744d4091fa1748f
SECT <info@sectwebstudio.com>
2017-08-22 20:32:01 +0900

    Add a package 'pixrem'

--- postcss.config.js
+++ postcss.config.js
@@ -18,6 +18,7 @@ module.exports = {
     }),
     require("autoprefixer")({
       browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"]
-    })
+    }),
+    require("pixrem")
   ]
 };

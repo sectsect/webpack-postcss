957a7410ec8c15d848852a50030880f74dbccda8
SECT <info@sectwebstudio.com>
2017-10-31 00:37:10 +0900

    Add npm package 'postcss-reporter'

--- postcss.config.js
+++ postcss.config.js
@@ -21,5 +21,8 @@ module.exports = {
       browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
     }),
     require('pixrem'),
+    require('postcss-reporter')({
+      positionless: 'last',
+    }),
   ],
 };

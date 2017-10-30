62f7f1113f0b66cc19fac05dabb9ff1d4db272ff
SECT <info@sectwebstudio.com>
2017-10-31 00:09:35 +0900

    Code cleanup for ESLint on postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   plugins: [
-    require("postcss-strip-inline-comments"),
+    require('postcss-strip-inline-comments'),
     require('postcss-partial-import')({
-      prefix: "_"
+      prefix: '_',
     }),
-    require("postcss-simple-vars")({
-      silent: true
+    require('postcss-simple-vars')({
+      silent: true,
     }),
-    require("postcss-css-variables"),
-    require("postcss-mixins"),
-    require("postcss-nested"),
+    require('postcss-css-variables'),
+    require('postcss-mixins'),
+    require('postcss-nested'),
     require('postcss-conditionals'),
-    require("postcss-for"),
-    require("postcss-extend"),
-    require("postcss-calc"),
-    require("css-mqpacker")({
-      sort: true
+    require('postcss-for'),
+    require('postcss-extend'),
+    require('postcss-calc'),
+    require('css-mqpacker')({
+      sort: true,
     }),
-    require("autoprefixer")({
-      browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"]
+    require('autoprefixer')({
+      browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
     }),
-    require("pixrem")
-  ]
+    require('pixrem'),
+  ],
 };

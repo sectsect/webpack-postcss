e9a657bee6a0295758d453bd968409b80165186d
SECT <info@sectwebstudio.com>
2017-10-31 01:45:52 +0900

    Code cleanup for ESLnit on postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,43 @@
+const postcssStripInlineComments = require('postcss-strip-inline-comments');
+const postcssPartialImport = require('postcss-partial-import')({
+  prefix: '_',
+});
+const postcssSimpleVars = require('postcss-simple-vars')({
+  silent: true,
+});
+const postcssCssVariables = require('postcss-css-variables');
+const postcssMixins = require('postcss-mixins');
+const postcssNested = require('postcss-nested');
+const postcssConditionals = require('postcss-conditionals');
+const postcssFor = require('postcss-for');
+const postcssExtend = require('postcss-extend');
+const postcssCalc = require('postcss-calc');
+const cssMqpacker = require('css-mqpacker')({
+  sort: true,
+});
+const autoprefixer = require('autoprefixer')({
+  browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
+});
+const pixrem = require('pixrem');
+const postcssReporter = require('postcss-reporter')({
+  positionless: 'last',
+});
+
 module.exports = {
   plugins: [
-    require('postcss-strip-inline-comments'),
-    require('postcss-partial-import')({
-      prefix: '_',
-    }),
-    require('postcss-simple-vars')({
-      silent: true,
-    }),
-    require('postcss-css-variables'),
-    require('postcss-mixins'),
-    require('postcss-nested'),
-    require('postcss-conditionals'),
-    require('postcss-for'),
-    require('postcss-extend'),
-    require('postcss-calc'),
-    require('css-mqpacker')({
-      sort: true,
-    }),
-    require('autoprefixer')({
-      browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4', 'ios_saf >= 8'],
-    }),
-    require('pixrem'),
-    require('postcss-reporter')({
-      positionless: 'last',
-    }),
+    postcssStripInlineComments,
+    postcssPartialImport,
+    postcssSimpleVars,
+    postcssCssVariables,
+    postcssMixins,
+    postcssNested,
+    postcssConditionals,
+    postcssFor,
+    postcssExtend,
+    postcssCalc,
+    cssMqpacker,
+    autoprefixer,
+    pixrem,
+    postcssReporter,
   ],
 };

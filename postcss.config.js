de205e8b2c10d3669375861859cbdf976f2a0064
SECT <info@sectwebstudio.com>
2017-11-22 02:24:49 +0900

    Replace npm package to convert px to rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
+const pxtorem = require('postcss-pxtorem')({
+  replace: false,
+});
 const cssMqpacker = require('css-mqpacker')({
   sort: true,
 });
 const autoprefixer = require('autoprefixer')({
   browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4', 'ios_saf >= 8'],
 });
-const pixrem = require('pixrem');
 
 module.exports = {
   plugins: [
+    pxtorem,
     cssMqpacker,
     autoprefixer,
-    pixrem,
   ],
 };

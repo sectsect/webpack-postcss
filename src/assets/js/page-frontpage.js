7128ca4e3ed914f1297b2d993cc42dabbe211acb
SECT <info@sectwebstudio.com>
2017-12-05 19:47:49 +0900

    Split non-common JS files for each page

--- src/assets/js/page-frontpage.js
+++ src/assets/js/page-frontpage.js
@@ -1,5 +1,42 @@
+import slick from 'slick-carousel';
+
 const hello = 'Hello!!';
 console.log(hello);
 
 const sect = 'SECT!!';
 console.log(sect);
+
+
+jQuery(() => {
+  jQuery('.slider_wrap').each(function () {
+    const parentid = `#${jQuery(this).attr('id')}`;
+    jQuery('.slider').on('init', function (event, slick) {
+      jQuery(this).closest('.slider_wrap').addClass('ready');
+    });
+    jQuery(`${parentid} .slider-for`).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 200,
+      arrows: true,
+      fade: false,
+      asNavFor: `${parentid} .slider-nav`,
+    });
+    jQuery(`${parentid} .slider-nav`).slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      speed: 200,
+      asNavFor: `${parentid} .slider-for`,
+      arrows: false,
+      dots: false,
+      //	centerMode		  : true,
+      focusOnSelect: true,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      }],
+    });
+  });
+});

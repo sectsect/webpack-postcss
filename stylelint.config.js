49caa758ac5c00e839ab0898626d5bff44831cea
SECT <info@sectwebstudio.com>
2019-08-14 21:20:05 +0900

    Tweak rules for stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,4 @@
+// @ https://stylelint.io/user-guide/example-config
 module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-prettier", "stylelint-config-recess-order"],
   plugins: ['stylelint-prettier'],
@@ -8,35 +9,14 @@ module.exports = {
         ignoreAtRules: ["mixin", "define-mixin", "if", "else"]
       }
     ],
-    // "block-no-empty": true,
-    // "color-no-invalid-hex": true,
-    // "comment-no-empty": true,
     // "declaration-block-no-duplicate-properties": [
     //   true,
     //   {
     //     ignore: ["consecutive-duplicates-with-different-values"]
     //   }
     // ],
-    // "declaration-block-no-shorthand-property-overrides": true,
-    "font-family-no-duplicate-names": null,
-    // "font-family-no-missing-generic-family-keyword": true,
-    // "function-calc-no-unspaced-operator": true,
-    // "function-linear-gradient-no-nonstandard-direction": true,
-    // "keyframe-declaration-no-important": true,
-    // "media-feature-name-no-unknown": true,
     "no-descending-specificity": null,
-    // "no-duplicate-at-import-rules": true,
     "no-duplicate-selectors": null,
-    // "no-empty-source": true,
-    // "no-extra-semicolons": true,
-    // "no-invalid-double-slash-comments": true,
-    "number-leading-zero": null,
-    // "property-no-unknown": true,
-    // "selector-pseudo-class-no-unknown": true,
-    // "selector-pseudo-element-no-unknown": true,
-    // "selector-type-no-unknown": true,
-    // "string-no-newline": true,
-    // "unit-no-unknown": true
     "prettier/prettier": true,
   }
 };

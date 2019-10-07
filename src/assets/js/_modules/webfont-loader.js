802c512610ac960b053cfa31e64b9a3c2bde00ea
SECT <info@sectwebstudio.com>
2019-10-08 04:38:00 +0900

    Change hosting the font "Noto Sans" to Google Web Fonts

--- src/assets/js/_modules/webfont-loader.js
+++ src/assets/js/_modules/webfont-loader.js
@@ -2,13 +2,14 @@ import WebFont from 'webfontloader';
 
 export default () => {
   WebFont.load({
-    custom: {
-      families: ['NotoSansCJKjp:n4,n7'],
-      urls: ['/assets/css/fonts.css'],
-      // urls: [`${wp_data.template_dir_uri}/assets/css/fonts.css?v=${wp_data.theme_version}`],
-    },
+    // custom: {
+    //   families: ['NotoSansCJKjp:n4,n7'],
+    //   urls: ['/assets/css/fonts.css'],
+    //   // urls: [`${wp_data.template_dir_uri}/assets/css/fonts.css?v=${wp_data.theme_version}`],
+    // },
     google: {
-      families: ['Josefin Sans:400,600,700'],
+      families: ['Noto+Sans+JP:400,700'],
+      // families: ['Josefin Sans:400,600,700'],
       // families: ['Raleway:300,400,500,600,700'],
       // families: ['Crimson+Text::latin', 'Cinzel::latin', 'Italianno::latin', 'Pinyon+Script::latin'],
     },

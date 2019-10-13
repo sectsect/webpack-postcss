e3e8c7e9d2b6fb17f148125f0d308a11dafe2c71
SECT <info@sectwebstudio.com>
2019-10-14 06:05:16 +0900

    Add example of class

---
+++ src/assets/ts/_class/myclass.ts
@@ -0,0 +1,38 @@
+/**
+ * ES6 Class
+ *
+ * @param string  msg   Where something interesting takes place
+ *
+ * @return string
+ */
+export default class {
+  name: string;
+
+  age: number;
+
+  city: string;
+
+  country: string;
+
+  email: string;
+
+  constructor(name = '', age = 20, city = '', country = '', email = '') {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+    this.country = country;
+    this.email = email;
+  }
+
+  toString(): string {
+    return `${this.name} | ${this.age} | ${this.city} | ${this.email}`;
+  }
+
+  addCountry(): string {
+    return `${this.toString()} | ${this.country}`;
+  }
+
+  run(): string {
+    return this.addCountry();
+  }
+}
--- src/assets/ts/page-frontpage.ts
+++ src/assets/ts/page-frontpage.ts
@@ -1,5 +1,6 @@
 import './_modules/commons';
 import glide from './_modules/glide';
+import MyClass from './_class/myclass';
 
 glide();
 
@@ -17,3 +18,36 @@ glide();
 // let message: String = '';
 // message = 2;
 // console.log(`Hello!${message}`);
+
+/**
+ * ES6 Class
+ *
+ * @param string  msg   Where something interesting takes place
+ *
+ * @return string
+ */
+// class MyClass {
+//   constructor(name, age, city, country, email) {
+//     this.name = name;
+//     this.age = age;
+//     this.city = city;
+//     this.country = country;
+//     this.email = email;
+//   }
+//
+//   toString() {
+//     return `${this.name} | ${this.age} | ${this.city} | ${this.email}`;
+//   }
+//
+//   addCountry() {
+//     return `${this.toString()} | ${this.country}`;
+//   }
+//
+//   run() {
+//     return this.addCountry();
+//   }
+// }
+const myClass = new MyClass('SECT', 37, 'Tokyo', 'Japan', 'info@xxxxxxxxx.com');
+const meta = myClass.run(); // 'SECT | 37 | Tokyo | info@xxxxxxxxx.com'
+console.log(meta);
+// console.log(myClass.email); // 'info@xxxxxxxxx.com'

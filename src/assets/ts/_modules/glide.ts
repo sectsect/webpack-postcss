23d60e8e352134b5a1a290a72707af30ff031327
SECT <info@sectwebstudio.com>
2019-11-02 14:46:00 +0900

    Update config for Typescript

--- src/assets/ts/_modules/glide.ts
+++ src/assets/ts/_modules/glide.ts
@@ -4,10 +4,39 @@ import { unveil } from './unveil-lazysizes';
 declare let jQuery: any;
 
 export const glide = (): void => {
-  jQuery('.glide_wrap').each(function(): void {
-    const c = jQuery(this).attr('data-glide-count');
+  // jQuery('.glide_wrap').each(function(this: JQuery): void {
+  //   const c = jQuery(this).attr('data-glide-count');
+  //   if (c !== '1') {
+  //     const glide = new Glide(this, {
+  //       type: 'carousel',
+  //       autoplay: 4000,
+  //       animationTimingFunc: 'ease-in-out',
+  //       animationDuration: 600,
+  //       hoverpause: false,
+  //       gap: 0,
+  //       perView: 1,
+  //       // breakpoints: {
+  //       //   767: {
+  //       //     perView: 2,
+  //       //   },
+  //       //   // 1024: {
+  //       //   //   perView: 1,
+  //       //   // },
+  //       // },
+  //     });
+  //     glide.on('mount.after', (): void => {
+  //       jQuery(this).addClass('ready');
+  //       unveil(jQuery(this).find('.glide__slide img'));
+  //     });
+  //     glide.mount();
+  //   } else {
+  //     jQuery(this).addClass('ready');
+  //   }
+  // });
+  jQuery('.glide_wrap').each((_i: number, el: HTMLElement): void => {
+    const c = jQuery(el).attr('data-glide-count');
     if (c !== '1') {
-      const glide = new Glide(this, {
+      const glide = new Glide(el, {
         type: 'carousel',
         autoplay: 4000,
         animationTimingFunc: 'ease-in-out',
@@ -25,12 +54,12 @@ export const glide = (): void => {
         // },
       });
       glide.on('mount.after', (): void => {
-        jQuery(this).addClass('ready');
-        unveil(jQuery(this).find('.glide__slide img'));
+        jQuery(el).addClass('ready');
+        unveil(jQuery(el).find('.glide__slide img'));
       });
       glide.mount();
     } else {
-      jQuery(this).addClass('ready');
+      jQuery(el).addClass('ready');
     }
   });
 };

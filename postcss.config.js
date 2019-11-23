852518a11a0d6ec8b8c2b9c9742d48814e5a6099
SECT <info@sectwebstudio.com>
2019-11-23 21:35:35 +0900

    Setup for 'postcss-preset-env'

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,29 @@
 const postcssImport = require('postcss-import');
-const postcssPresetEnv = require('postcss-preset-env')({
-  // stage: 3,  // Default: stage: 2   @ https://cssdb.org/#staging-process
-});
-const postcssSimpleVars = require('postcss-simple-vars')({
-  silent: true,
-});
 const postcssStripInlineComments = require('postcss-strip-inline-comments');
-const postcssCssVariables = require('postcss-css-variables');
-const postcssMixins = require('postcss-mixins');
-const postcssNested = require('postcss-nested');
-const postcssConditionals = require('postcss-conditionals');
-const postcssFor = require('postcss-for');
-const postcssExtend = require('postcss-extend');
-const postcssCalc = require('postcss-calc');
-const postcssClearfix = require('postcss-clearfix');
-const pxtorem = require('postcss-pxtorem')({
-  replace: false,
-});
-const postcssHexrgba = require('postcss-hexrgba');
-const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+// const postcssMixins = require('postcss-mixins');
+// const postcssNested = require('postcss-nested');
+// const postcssConditionals = require('postcss-conditionals');
+// const postcssFor = require('postcss-for');
+// const postcssExtend = require('postcss-extend');
 const cssMqpacker = require('css-mqpacker')({
   sort: true,
 });
-const autoprefixer = require('autoprefixer')({
-  grid: 'autoplace',
+const postcssPresetEnv = require('postcss-preset-env')({
+  stage: 1,  // Default: stage: 2   @ https://cssdb.org/#staging-process
+  autoprefixer: {
+    grid: 'autoplace'
+  },
+  features: {
+    'nesting-rules': true
+  }
 });
-const postcssSorting = require('postcss-sorting')({
-  'properties-order': 'alphabetical',
+const pxtorem = require('postcss-pxtorem')({
+  replace: false,
 });
-const validator = require('postcss-validator');
+const postcssCalc = require('postcss-calc');
+const postcssClearfix = require('postcss-clearfix');
+const postcssHexrgba = require('postcss-hexrgba');
+const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
 const postcssReporter = require('postcss-reporter')({
   positionless: 'last',
 });
@@ -37,23 +32,18 @@ module.exports = {
   plugins: [
     postcssImport,
     postcssStripInlineComments,
+    // postcssMixins,
+    // postcssNested,
+    // postcssConditionals,
+    // postcssFor,
+    // postcssExtend,
+    cssMqpacker,
     postcssPresetEnv,
-    postcssSimpleVars,
-    postcssCssVariables,
-    postcssMixins,
-    postcssNested,
-    postcssConditionals,
-    postcssFor,
-    postcssExtend,
+    pxtorem,
     postcssCalc,
     postcssClearfix,
-    pxtorem,
     postcssHexrgba,
     postcssFlexbugsFixes,
-    cssMqpacker,
-    autoprefixer,
-    postcssSorting,
-    validator,
     postcssReporter,
   ],
 };

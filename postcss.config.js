33e27377cced59cbdc6c089311a02f640f36f8da
SECT <info@sectwebstudio.com>
2019-11-24 18:14:11 +0900

    Revert "Merge branch 'dev' into postcss-similar-to-sass"
    This reverts commit 4d6b6003e57063b6d8e2c6f485ab17ac01141c94, reversing
    changes made to a4ecd8136e059afbf46440be687a4fd7b98aa854.

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,31 @@
 const postcssImport = require('postcss-import');
-const postcssPresetEnv = require('postcss-preset-env')({
-  stage: 1,  // Default: stage: 2   @ https://cssdb.org/#staging-process
-  autoprefixer: {
-    grid: 'autoplace'
-  },
-  features: {
-    'nesting-rules': true
-  }
+const postcssSimpleVars = require('postcss-simple-vars')({
+  silent: true,
 });
-const cssMqpacker = require('css-mqpacker')({
-  sort: true,
-});
+const postcssStripInlineComments = require('postcss-strip-inline-comments');
+const postcssCssVariables = require('postcss-css-variables');
+const postcssMixins = require('postcss-mixins');
+const postcssNested = require('postcss-nested');
+const postcssConditionals = require('postcss-conditionals');
+const postcssFor = require('postcss-for');
+const postcssExtend = require('postcss-extend');
+const postcssCalc = require('postcss-calc');
+const postcssClearfix = require('postcss-clearfix');
 const pxtorem = require('postcss-pxtorem')({
   replace: false,
 });
-const postcssCalc = require('postcss-calc');
-const postcssClearfix = require('postcss-clearfix');
 const postcssHexrgba = require('postcss-hexrgba');
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+const cssMqpacker = require('css-mqpacker')({
+  sort: true,
+});
+const autoprefixer = require('autoprefixer')({
+  grid: 'autoplace',
+});
+const postcssSorting = require('postcss-sorting')({
+  'properties-order': 'alphabetical',
+});
+const validator = require('postcss-validator');
 const postcssReporter = require('postcss-reporter')({
   positionless: 'last',
 });
@@ -25,13 +33,23 @@ const postcssReporter = require('postcss-reporter')({
 module.exports = {
   plugins: [
     postcssImport,
-    postcssPresetEnv,
-    cssMqpacker,
-    pxtorem,
+    postcssStripInlineComments,
+    postcssSimpleVars,
+    postcssCssVariables,
+    postcssMixins,
+    postcssNested,
+    postcssConditionals,
+    postcssFor,
+    postcssExtend,
     postcssCalc,
     postcssClearfix,
+    pxtorem,
     postcssHexrgba,
     postcssFlexbugsFixes,
+    cssMqpacker,
+    autoprefixer,
+    postcssSorting,
+    validator,
     postcssReporter,
   ],
 };

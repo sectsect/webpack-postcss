a38bee549b380520f814faf80a8c4a1d0ff7186c
SECT <info@sectwebstudio.com>
2019-11-27 13:55:54 +0900

    Add example for 'vue-router'

---
+++ src/assets/js/page-example.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import './_modules/commons';
+import router from '../../router';
+import App from '../../components/App.vue';
+
+const app = new Vue({
+  el: '#app',
+  components: { App },
+  router,
+  render: h => h(App),
+});

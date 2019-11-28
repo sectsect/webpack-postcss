3c0bf3c005430eecfd7cf52331f2b22837d55eed
SECT <info@sectwebstudio.com>
2019-11-28 21:14:24 +0900

    Fix script for slider

--- src/assets/ts/_modules/glide.ts
+++ src/assets/ts/_modules/glide.ts
@@ -34,32 +34,27 @@ export const glide = (): void => {
   //   }
   // });
   jQuery('.glide_wrap').each((_i: number, el: HTMLElement): void => {
-    const c = jQuery(el).attr('data-glide-count');
-    if (c !== '1') {
-      const glide = new Glide(el, {
-        type: 'carousel',
-        autoplay: 4000,
-        animationTimingFunc: 'ease-in-out',
-        animationDuration: 600,
-        hoverpause: false,
-        gap: 0,
-        perView: 1,
-        // breakpoints: {
-        //   767: {
-        //     perView: 2,
-        //   },
-        //   // 1024: {
-        //   //   perView: 1,
-        //   // },
-        // },
-      });
-      glide.on('mount.after', (): void => {
-        jQuery(el).addClass('ready');
-        unveil(jQuery(el).find('.glide__slide img'));
-      });
-      glide.mount();
-    } else {
+    const glide = new Glide(el, {
+      type: 'carousel',
+      autoplay: 4000,
+      animationTimingFunc: 'ease-in-out',
+      animationDuration: 600,
+      hoverpause: false,
+      gap: 0,
+      perView: 1,
+      // breakpoints: {
+      //   767: {
+      //     perView: 2,
+      //   },
+      //   // 1024: {
+      //   //   perView: 1,
+      //   // },
+      // },
+    });
+    glide.on('mount.after', (): void => {
       jQuery(el).addClass('ready');
-    }
+      unveil(jQuery(el).find('.glide__slide img'));
+    });
+    glide.mount();
   });
 };

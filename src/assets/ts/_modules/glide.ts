78bd027b49a4d3a84f32b3fa320ff6b5ee1b7338
SECT <info@sectwebstudio.com>
2020-01-19 01:48:55 +0900

    Improve handling for Glide slider and image lazy loading

--- src/assets/ts/_modules/glide.ts
+++ src/assets/ts/_modules/glide.ts
@@ -4,36 +4,7 @@ import { unveil } from './unveil-lazysizes';
 declare let jQuery: any;
 
 export const glide = (): void => {
-  // jQuery('.glide_wrap').each(function(this: JQuery): void {
-  //   const c = jQuery(this).attr('data-glide-count');
-  //   if (c !== '1') {
-  //     const glide = new Glide(this, {
-  //       type: 'carousel',
-  //       autoplay: 4000,
-  //       animationTimingFunc: 'ease-in-out',
-  //       animationDuration: 600,
-  //       hoverpause: false,
-  //       gap: 0,
-  //       perView: 1,
-  //       // breakpoints: {
-  //       //   767: {
-  //       //     perView: 2,
-  //       //   },
-  //       //   // 1024: {
-  //       //   //   perView: 1,
-  //       //   // },
-  //       // },
-  //     });
-  //     glide.on('mount.after', (): void => {
-  //       jQuery(this).addClass('ready');
-  //       unveil(jQuery(this).find('.glide__slide img'));
-  //     });
-  //     glide.mount();
-  //   } else {
-  //     jQuery(this).addClass('ready');
-  //   }
-  // });
-  jQuery('.glide_wrap').each((_i: number, el: HTMLElement): void => {
+  const runSlider = (el: HTMLElement) => {
     const glide = new Glide(el, {
       type: 'carousel',
       autoplay: 4000,
@@ -53,8 +24,22 @@ export const glide = (): void => {
     });
     glide.on('mount.after', (): void => {
       jQuery(el).addClass('ready');
-      unveil(jQuery(el).find('.glide__slide img'));
     });
     glide.mount();
+  };
+
+  const asyncSlider = async (self: HTMLElement) => {
+    try {
+      const result = await unveil(jQuery(self).find('figure img'));
+      if (result) {
+        runSlider(self);
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  jQuery('.glide_wrap').each((_i: number, el: HTMLElement): void => {
+    asyncSlider(el);
   });
 };

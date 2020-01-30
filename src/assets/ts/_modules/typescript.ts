c4dd822b089a08d02033459456b14cf8664a18fd
SECT <info@sectwebstudio.com>
2020-01-31 05:03:43 +0900

    Add example code for 'Literal Types'

--- src/assets/ts/_modules/typescript.ts
+++ src/assets/ts/_modules/typescript.ts
@@ -1,7 +1,7 @@
 // declare let jQuery: any;
 
 export const typescript = () => {
-  // 列挙型 (Enums)
+  // Enums
   // @ https://typescript-jp.gitbook.io/deep-dive/type-system/enums
   enum Role {
     ADMIN = 'ADMIN',
@@ -19,4 +19,37 @@ export const typescript = () => {
   if (person.role === Role.ADMIN) {
     console.log('is admin');
   }
+
+  // Literal Types
+  // @ https://typescript-jp.gitbook.io/deep-dive/type-system/literal-types
+  function combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text',
+  ) {
+    let result;
+    if (
+      (typeof input1 === 'number' && typeof input2 === 'number') ||
+      resultConversion === 'as-number'
+    ) {
+      result = +input1 + +input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
+  }
+
+  const combinedAges = combine(30, 26, 'as-number');
+  console.log(combinedAges);
+
+  const combinedStringAges = combine('30', '26', 'as-number');
+  console.log(combinedStringAges);
+
+  const combinedNames = combine('Max', 'Anna', 'as-text');
+  console.log(combinedNames);
 };

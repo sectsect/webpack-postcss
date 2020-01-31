0cdc9e71bbfa812f30969aab66dc5aedb4668787
SECT <info@sectwebstudio.com>
2020-01-31 22:11:41 +0900

    Fix scipt for Modal-Window

--- src/assets/ts/_modules/modal.ts
+++ src/assets/ts/_modules/modal.ts
@@ -1,4 +1,7 @@
+import 'core-js/modules/es.object.assign';
+import 'core-js/modules/es.array.from';
 import MicroModal from 'micromodal';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export const modal = () => {
   MicroModal.init({
@@ -6,9 +9,19 @@ export const modal = () => {
     // onClose: modal => console.info(`${modal.id} is hidden`), // [2]
     // openTrigger: 'data-custom-open', // [3]
     // closeTrigger: 'data-custom-close', // [4]
-    disableScroll: true,
+    // disableScroll: true,
     disableFocus: false, // [6]
     awaitCloseAnimation: true, // [7]
     // debugMode: true, // [8]
+    onShow: modal => {
+      if (modal && modal.id) {
+        disableBodyScroll(document.querySelector(`#${modal.id}`) as HTMLElement);
+      }
+    },
+    onClose: modal => {
+      if (modal && modal.id) {
+        enableBodyScroll(document.querySelector(`#${modal.id}`) as HTMLElement);
+      }
+    },
   });
 };

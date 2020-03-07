727d88cb77f1986b46a0268a1c278d1c4c5b45dc
SECT <info@sectwebstudio.com>
2020-03-08 02:10:12 +0900

    Add example code for 'Function Types & Callbacks'

--- src/assets/ts/_modules/typescript.ts
+++ src/assets/ts/_modules/typescript.ts
@@ -52,4 +52,35 @@ export const typescript = () => {
 
   const combinedNames = combine('Max', 'Anna', 'as-text');
   console.log(combinedNames);
+
+  // Function Types & Callbacks
+  function add(n1: number, n2: number) {
+    return n1 + n2;
+  }
+
+  function printResult(num: number): void {
+    console.log(`Result: ${num}`);
+  }
+
+  function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+  }
+
+  printResult(add(5, 12));
+
+  let combineValues: (a: number, b: number) => number;
+
+  // eslint-disable-next-line prefer-const
+  combineValues = add;
+  // combineValues = printResult;
+  // combineValues = 5;
+
+  console.log(combineValues(8, 8));
+
+  // let someValue: undefined;
+
+  addAndHandle(10, 20, result => {
+    console.log(result);
+  });
 };

7403e26e53f86ed8d648bf6da79abf5b2880a38c
SECT <info@sectwebstudio.com>
2020-03-08 17:49:03 +0900

    Improve TypeScript syntax

--- src/assets/ts/_modules/detect.ts
+++ src/assets/ts/_modules/detect.ts
@@ -42,40 +42,37 @@ interface Htmlcss {
   objectfit: boolean;
 }
 
-export const detection = (): Detection | undefined => {
-  if (browser?.name && browser?.version) {
-    const bn: string = browser.name;
-    const bv: number = parseInt(browser.version, 10);
-    const detect: Detection = {
-      device: {
-        desktop: !isMobile().any,
-        mobile: isMobile().any,
-        phone: isMobile().phone,
-        tablet: isMobile().tablet,
-        chrome: bn === 'chrome' ? bv : false,
-        firefox: bn === 'firefox' ? bv : false,
-        safari: bn === 'safari' ? bv : false,
-        msie: bn === 'ie' ? bv : false,
-        msedge: bn === 'edge' ? bv : false,
-        iPhone: isMobile().apple.phone,
-        androidphone: isMobile().android.phone,
-        iOS: isMobile().apple.device,
-        androidOS: isMobile().android.device,
-      },
-      htmlcss: {
-        cssgrid: Modernizr.cssgrid,
-        flexbox: Modernizr.flexbox,
-        flexboxtweener: Modernizr.flexboxtweener,
-        objectfit: Modernizr.objectfit,
-      },
-      breakpoints: {
-        sm: 768,
-        md: 992,
-        lg: 1920,
-        xl: 2048,
-      },
-    };
-    return detect;
-  }
-  return undefined;
+export const detection = (): Detection => {
+  const bn: string = browser?.name || '';
+  const bv: number = parseInt(browser?.version || '0', 10);
+  const detect: Detection = {
+    device: {
+      desktop: !isMobile().any,
+      mobile: isMobile().any,
+      phone: isMobile().phone,
+      tablet: isMobile().tablet,
+      chrome: bn === 'chrome' ? bv : false,
+      firefox: bn === 'firefox' ? bv : false,
+      safari: bn === 'safari' ? bv : false,
+      msie: bn === 'ie' ? bv : false,
+      msedge: bn === 'edge' ? bv : false,
+      iPhone: isMobile().apple.phone,
+      androidphone: isMobile().android.phone,
+      iOS: isMobile().apple.device,
+      androidOS: isMobile().android.device,
+    },
+    htmlcss: {
+      cssgrid: Modernizr.cssgrid,
+      flexbox: Modernizr.flexbox,
+      flexboxtweener: Modernizr.flexboxtweener,
+      objectfit: Modernizr.objectfit,
+    },
+    breakpoints: {
+      sm: 768,
+      md: 992,
+      lg: 1920,
+      xl: 2048,
+    },
+  };
+  return detect;
 };

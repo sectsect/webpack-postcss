fd73ab2be671dc257dfba6c90954ec128b162be1
SECT <info@sectwebstudio.com>
2021-07-04 17:54:03 +0900

    Add 'thread-loader'

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,14 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const dotenv = require('dotenv').config();
 const { WebpackSweetEntry } = require('@sect/webpack-sweet-entry');
 const NotifierPlugin = require('@soda/friendly-errors-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const notifier = require('node-notifier');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const SizePlugin = require('size-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const threadLoader = require('thread-loader');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 // const SpritesmithPlugin = require('webpack-spritesmith');
@@ -21,6 +22,27 @@ const buildPath = path.join(__dirname, 'dist');
 
 // For dotenv
 // console.log(process.env.AWS_ACCESS_KEY_ID);
+
+// thread-loader
+const jsWorkerPoolOptions = {
+  // workers: 1,
+  workerParallelJobs: 50,
+  // workerNodeArgs: ['--max-old-space-size=1024'],
+  poolTimeout: 1000,
+  name: 'js-loader-pool',
+};
+const cssWorkerPoolOptions = {
+  // workers: 1,
+  workerParallelJobs: 50,
+  // workerNodeArgs: ['--max-old-space-size=1024'],
+  poolTimeout: 1000,
+  name: 'css-loader-pool',
+};
+
+// webpack build hangs with warmup and new cache API
+// @ https://github.com/webpack-contrib/thread-loader/issues/122
+// threadLoader.warmup(jsWorkerPoolOptions, ['babel-loader']);
+// threadLoader.warmup(cssWorkerPoolOptions, ['css-loader', 'postcss-loader']);
 
 // For Detection Environment  @ https://webpack.js.org/api/cli/#environment-options
 const isProd = env => env && env.production;
@@ -208,6 +230,10 @@ module.exports = env => [
           // exclude: /node_modules\/(?!(rambda|quicklink)\/).*/,
           use: [
             {
+              loader: 'thread-loader',
+              options: jsWorkerPoolOptions,
+            },
+            {
               loader: 'babel-loader',
               options: {
                 cacheDirectory: true,
@@ -290,6 +316,10 @@ module.exports = env => [
           use: [
             MiniCssExtractPlugin.loader,
             {
+              loader: 'thread-loader',
+              options: cssWorkerPoolOptions,
+            },
+            {
               loader: 'css-loader',
               options: {
                 url: false,
@@ -312,7 +342,7 @@ module.exports = env => [
               'default',
               {
                 discardComments: {
-                  removeAll: true
+                  removeAll: true,
                 },
               },
             ],

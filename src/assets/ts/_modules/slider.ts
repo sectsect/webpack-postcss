1e6526968caebe8add5f96644fd33003203a8b04
SECT <info@sectwebstudio.com>
2021-10-08 17:33:58 +0900

    Improve handling to await images loads on slider initialize

--- src/assets/ts/_modules/slider.ts
+++ src/assets/ts/_modules/slider.ts
@@ -15,20 +15,45 @@ export const slider = (): void => {
    */
   const runSlider = (parentid: string) => {
     const $slickElement = $(`${parentid} .slider-for`);
-    $slickElement.on(
-      'init',
-      (
-        e,
-        // _event: any,
-        // _slick: { slideCount: number },
-        // _currentSlide: any,
-        // _nextSlide: any,
-      ) => {
+
+    $slickElement.on('init', e => {
+      // $slickElement.on('init', (e, slick: { slideCount: number }) => {
+      // Re-execute 'unveil' on lazyload for missing image in last slide on 'infinite' mode
+      unveil($(e.currentTarget).find('figure img'));
+      setTimeout(() => {
         $(e.currentTarget).closest('.slider_wrap').addClass('ready');
-        // Re-execute 'unveil' on lazyload  for missing image in last slide on infinite mode
-        unveil($(e.currentTarget).find('figure img'));
+      }, 300);
+    });
+
+    // Restart autoplay (Slick Slider autoplay stops when touched on mobile/tablet)
+    // @ https://stackoverflow.com/questions/48402481/slick-slider-autoplay-stops-when-touched-on-mobile-tablet
+    $(`${parentid} .slider`).on('touchstart', e => {
+      $(e.currentTarget).slick('slickPlay');
+    });
+
+    // Restart autoplay (Autoplay is stopped after clicking the slick-dot)
+    // @ https://github.com/kenwheeler/slick/issues/2655#issuecomment-313923749
+    $(`${parentid} .slider`).on(
+      'beforeChange',
+      // (_event: any, _slick: any, _currentSlide: number, _nextSlide: any) => {
+      () => {
+        $(`${parentid} .slider .slick-dots li`).removeClass('slick-active');
+        $(`${parentid} .slider .slick-dots li button`)
+          .attr('aria-pressed', 'false')
+          .focus(e => {
+            e.currentTarget.blur();
+          });
       },
     );
+
+    // $slickElement.on(
+    //   'afterChange',
+    //   (_event: any, _slick: any, currentSlide: number, _nextSlide: any) => {
+    //     // For GA Tracking
+    //     const slidenum: number = currentSlide + 1;
+    //     const slider: string = parentid.replace('#slider_', '');
+    //   },
+    // );
 
     $(`${parentid} .slider-for`).slick({
       slidesToShow: 1,
@@ -59,36 +84,6 @@ export const slider = (): void => {
       //     }
       // }]
     });
-
-    // Restart autoplay (Slick Slider autoplay stops when touched on mobile/tablet)
-    // @ https://stackoverflow.com/questions/48402481/slick-slider-autoplay-stops-when-touched-on-mobile-tablet
-    $(`${parentid} .slider`).on('touchstart', (e: { currentTarget: HTMLElement }) => {
-      $(e.currentTarget).slick('slickPlay');
-    });
-
-    // Restart autoplay (Autoplay is stopped after clicking the slick-dot)
-    // @ https://github.com/kenwheeler/slick/issues/2655#issuecomment-313923749
-    $(`${parentid} .slider`).on(
-      'beforeChange',
-      // (_event: any, _slick: any, _currentSlide: number, _nextSlide: any) => {
-      () => {
-        $(`${parentid} .slider .slick-dots li`).removeClass('slick-active');
-        $(`${parentid} .slider .slick-dots li button`)
-          .attr('aria-pressed', 'false')
-          .focus(e => {
-            e.currentTarget.blur();
-          });
-      },
-    );
-
-    // $slickElement.on(
-    //   'afterChange',
-    //   (_event: any, _slick: any, currentSlide: number, _nextSlide: any) => {
-    //     // For GA Tracking
-    //     const slidenum: number = currentSlide + 1;
-    //     const slider: string = parentid.replace('#slider_', '');
-    //   },
-    // );
   };
 
   /**
@@ -106,6 +101,9 @@ export const slider = (): void => {
         await loadImage(images)
           .then(allImgs => {
             console.log(allImgs.length, 'images loaded!', allImgs);
+            allImgs.forEach(img => {
+              console.log($(img).attr('data-srcset'));
+            });
           })
           .catch((e: unknown) => {
             // console.error('One or more images have failed to load :(');

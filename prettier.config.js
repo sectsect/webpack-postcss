98969de65d4d32edbf7ec61861a4b866782eb017
sect <sect@sect.me>
2023-12-03 17:46:16 +0900

    chore: update dependencies

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // eslint-disable-next-line global-require
-  ...require('@spotify/prettier-config'),
-  // your overrides here
-  printWidth: 100,
+  // ...require('@spotify/prettier-config'),
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
 };
